551b464ca8823e859e4df4e2ce3f173a221a2faf
Cristian Sotomayor <cristiansotomayor.dev@gmail.com>
2022-12-03 09:19:42 -0500

    actualizando service

--- client/src/app/app.service.ts
+++ client/src/app/app.service.ts
@@ -1,13 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, map, Observable, tap } from 'rxjs';
-import { IProduct } from './app.interfaces';
+import { IProduct, ISelectedProduct } from './app.interfaces';
 
 @Injectable({providedIn: 'root'})
 export class AppService {
   configUrl = 'http://localhost:3000';
   products$: BehaviorSubject<IProduct[]> = new BehaviorSubject<IProduct[]>([]);
   filter$: BehaviorSubject<string> = new BehaviorSubject('');
+  selectedProducts$: BehaviorSubject<ISelectedProduct[]> = new BehaviorSubject<ISelectedProduct[]>([]);
 
   constructor(private http: HttpClient) { }
 
@@ -15,11 +16,22 @@ export class AppService {
     return this.http.get<IProduct[]>(`${this.configUrl}/products`);
   }
 
+  // getSelectedProducts(): Observable<IProduct[]> {
+  //   return this.http.get<IProduct[]>(`${this.configUrl}/products`);
+  // }
+
+  addSelectedProduct(product: ISelectedProduct) {
+    // add selected products to the behavior subject
+    this.selectedProducts$.next([...this.selectedProducts$.value, product]);
+    // add to the local storage
+    localStorage.setItem('selectedProducts', JSON.stringify([...this.selectedProducts$.value, product]));
+  }
+
   set filter(value: string) {
     this.filter$.next(value);
   }
 
-  getProductsObservable() {
+  getProductsObservable(): Observable<IProduct[]> {
     return this.products$.pipe(
       tap((products) => {
         if (!products.length) {
